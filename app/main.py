db2c0565e6ea7c5c4182cce62164b42de7451232
Suphanat Rueanthai <suphanat1722@gmail.com>
2026-09-04 12:44:26 +0700

    Prompt v2 slot-fill translation guidance; release 1.6.1

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ from .core.logging import configure_logging
 from .repositories import database
 from .services.worker import worker
 
-APP_VERSION = "1.6.0"
+APP_VERSION = "1.6.1"
 
 
 @asynccontextmanager
